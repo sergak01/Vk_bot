cf0e6c84d342e471121b33229220a5bdfab7e08d
sergak01 <sergak.shpak@gmail.com>
2017-06-27 10:33:16 +0300

    Обновление класса dynamic_settings, исправление алгоритма сохранения динамических настроек, а также алгоритма считывания настроек. Добавление описаний к некоторым функциям

--- dynamic_settings.py
+++ dynamic_settings.py
@@ -30,6 +30,10 @@ class DynamicSettings:
 		self.dynamic_settings_filename = filename
 
 	def open_file(self, parametr = "r"):
+		'''Немного модифицированная стандартная функция открытия файла. Дает классу знание об открытости файла для записи.
+		Возвращает ссылку на файл, если с ним нужны манипуляции вне класса. Внутри класса файл будет открыт до вызова команды
+		close_file() из экземпляра класса.
+		Warning: Не закрывайте файл стандартной функцией close() через его идентификатор! Это, скорее всего, приведет к парочке исключений!'''
 		if parametr == "":
 			raise NameError("Parametr must be not empty!")
 		self.file = open(self.dynamic_settings_filename, parametr)
@@ -37,10 +41,17 @@ class DynamicSettings:
 		return self.file
 
 	def close_file(self):
+		'''Функция закрытия файла динамических настроек.'''
 		self.file.close()
 		self.file_opened = False
 
 	def get_dynamic_settings(self):
+		'''Функция для получения динамических настроек с файла. Возвращает словарь, который содержит все переменные, что находяться в файле.
+		Этот словарь будет актуальным тольки при запуске программы или инициализации класса. Перед вызовом стоит убедиться что файл существует
+		и он не пустой. Для этого вызываем функцию file_created() из экземпляра класса. Если она возвращает False, генерируем не пустой dict()
+		и передаем его экземпляру класса через функцию set_dynamic_settings().
+		Во время работы программы, если мы точно знаем что файл не исчез, функция используеться для обновления массива строк с файла. 
+		Каждый запуск обновляет переменную file_data_list, экземпляра класса.'''
 		if os.stat(self.dynamic_settings_filename).st_size != 0:
 			self.open_file("r")
 			if self.file_opened == False:
@@ -48,13 +59,14 @@ class DynamicSettings:
 				raise NameError('File must be opened!')
 			else:
 				self.file_data_list = self.file.readlines()
+				parameters_dict_temp = self.parameters_dict.copy()
 				for item in self.file_data_list:
-					if item[0] != '#' and '\n' and ' ':
-						rexp = r'([^ \n\t]+)[^=]*=[^\S]*([^\n]+)'
+					if item[0] != '#' and item[0] != '\n' and item[0] != ' ':
+						rexp = r'([^ \n\t]+)[^=]*=[^\S]*([^\n]+)\n*'
 						parser = re.compile(rexp)
-						self.parameters_dict.update({parser.sub(r'\1', str(item)): parser.sub(r'\2', str(item))})
+						parameters_dict_temp.update({parser.sub(r'\1', str(item)): parser.sub(r'\2', str(item))})
 				self.close_file()
-				return self.parameters_dict
+				return parameters_dict_temp
 		else:
 			raise NameError("File is empty!")
 
@@ -74,6 +86,7 @@ class DynamicSettings:
 			else:
 				for name, value in this_dict.items():
 					self.file.write(str(name) + " = " + str(value))
+					self.file.write('\n')
 				self.close_file()
 		else:
 			if self.get_dynamic_settings() == this_dict:
@@ -81,14 +94,33 @@ class DynamicSettings:
 			else:
 				self.get_dynamic_settings()
 				for name, value in this_dict.items():
-					self.parameters_dict.update(name, value)
+					self.parameters_dict.update({name: value})
 				self.open_file("w")
 				if self.file_opened == False:
 					self.close_file()
 					raise NameError("File not opened for writing!")
 				else:
-					for name, value in self.parameters_dict.items():
-						self.file.write(str(name) + " = " + str(value))
+					parameters_dict_temp = self.parameters_dict.copy()
+					for item in self.file_data_list:
+						rexp_comment = r'^[#\s][^\n]+\n'
+						parser_c = re.compile(rexp_comment)
+						# print(str(item))
+						if parser_c.sub("", str(item)) == "" or str(item) == "\n":
+							self.file.write(item)
+						else:
+							rexp = r'([^\s]+)[^\S]*=[^\S]*([^\n]+)\n?'
+							parser = re.compile(rexp)
+							for name, value in self.parameters_dict.items():
+								if name == parser.sub(r'\1', str(item)) and name != '':
+									# print(value)
+									self.file.write(str(name) + " = " + str(value))
+									self.file.write('\n')
+									parameters_dict_temp.pop(name, str(""))
+									break;
+					for name, value in parameters_dict_temp.items():
+						if name != "":
+							self.file.write(str(name) + " = " + str(value))
+							self.file.write('\n')
 					self.close_file()
 
 
--- start.py
+++ start.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import time
 from dynamic_settings import DynamicSettings
+import ast
 
 import settings
 
 def main():
+	'''Этот файл планируеться как основной стартовый модуль бота, который будет манипулировать файлом динамических настроек бота.
+	На этой стадии файл динамических настроек включает такие переменные:
+	users_token: тип данных dict - используеться для хранения временных токенов пользователей (планируеться использовать токены с временем
+	жизни в 1 час), которые, в свою очередь, подтверждрают наличие у пользователя прав администратора.'''
 	dyn_sett_dict = {}
 	dyn_sett_file = DynamicSettings(dynamic_settings_filename = settings.dynamic_settings_file)
 	if(dyn_sett_file.file_created()):
@@ -12,6 +17,10 @@ def main():
 	else:
 		dyn_sett_dict = generate_standart_dynamic_setting()
 		dyn_sett_file.set_dynamic_settings(dyn_sett_dict)
+	temp_token_list = ast.literal_eval(dyn_sett_dict.get("users_token"))
+	temp_token_list.update({50583928: "{user_token: token, time_out: 10}"})
+	dyn_sett_dict.update({"users_token" : temp_token_list})
+	dyn_sett_file.set_dynamic_settings(dyn_sett_dict)
 	# while True:
 	# 	setting = settings()
 	# 	print_settings(setting)
@@ -21,9 +30,9 @@ def main():
 	print(str(dyn_sett_dict))
 
 def print_settings(obj_settings):
+	print(str(getattr(obj_settings, "users_token")))
 	print(str(getattr(obj_settings, "answer_admins")))
 	print(str(getattr(obj_settings, "mail_subscription_admins")))
-	print(str(getattr(obj_settings, "mail_admin_online")))
 
 def generate_standart_dynamic_setting():
 	temp_dict = dict()
