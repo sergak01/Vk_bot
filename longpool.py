2eaabaaceca49c3ec7590758da2cf027ae6b803c
sergak01 <sergak.shpak@gmail.com>
2017-07-19 20:42:41 +0300

    Добавление цыкла перезапуска бота

--- longpool.py
+++ longpool.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import vk_api
 import re
+import time
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 import settings
@@ -11,7 +12,7 @@ def write_msg(user_id, s, vk_session, forward_messages=""):
     else:
         vk_session.method('messages.send', {'user_id':user_id,'message':s,'forward_messages':forward_messages})
 
-def main():
+def main(longpoll=None):
     try:
         """ Пример использования longpoll
             https://vk.com/dev/using_longpoll
@@ -31,79 +32,88 @@ def main():
         except vk_api.AuthError as error_msg:
             print(error_msg)
             return
-
-        longpoll = VkLongPoll(vk_session)
+        if longpoll == None:
+            longpoll = VkLongPoll(vk_session)
+        else:
+            longpoll.update_longpoll_server()
 
         for event in longpoll.listen():
+            try:
+                if event.type == VkEventType.MESSAGE_NEW:
+                    print('Новое сообщение:')
 
-            if event.type == VkEventType.MESSAGE_NEW:
-                print('Новое сообщение:')
+                    if event.from_me:
+                        print('От меня для: ', end='')
+                    elif event.to_me:
+                        print('Для меня от: ', end='')
 
-                if event.from_me:
-                    print('От меня для: ', end='')
-                elif event.to_me:
-                    print('Для меня от: ', end='')
-
-                if event.from_user:
-                    print(event.user_id)
-                    if int(event.user_id) == 57008075:
-                        reg = r'(id)?(\d*)? ?([^$]+)'
-                        parser=re.compile(reg)
-                        answer = parser.sub(r'\2', str(event.text))
-                        if answer != "":
+                    if event.from_user:
+                        print(event.user_id)
+                        if int(event.user_id) == 57008075:
+                            reg = r'(id)?(\d*)? ?([^$]+)'
+                            parser=re.compile(reg)
                             answer = parser.sub(r'\2', str(event.text))
-                            try:
-                                if parser.sub(r'\3', str(event.text)) != "":
-                                    write_msg(int(answer), parser.sub(r'\3', str(event.text)), vk_session)
-                                else:
-                                    write_msg(int(answer), u".", vk_session)
-                            except Exception as e:
-                                print(e)
-                            user_name = vk_empty_session.method('users.get', {'user_id':int(answer)})
-                            write_msg(57008075, u'Сообщение отправлено пользователю [id' + str(answer) + u'|' + 
-                                str(user_name[0]["first_name"]) + " " + str(user_name[0]["last_name"]) + 
-                                ']', vk_session)
-                            continue;
-                elif event.from_chat:
-                    print(event.user_id, 'в беседе', event.chat_id)
-                elif event.from_group:
-                    print('группы', event.group_id)
-                try:
-                    print('Текст: ', event.text)
-                except Exception as e:
-                    print(e)
-                print()
-                if not event.from_me:
-                    user_name = vk_empty_session.method('users.get', {'user_id':int(event.user_id)})
-                    write_msg(57008075, u'[id' + str(event.user_id) + u'|' + 
-                        str(user_name[0]["first_name"]) + " " + 
-                        str(user_name[0]["last_name"]) +'] написал: ' + 
-                        str(event.text) + '\nОтветить: https://vk.com/gim90818758?sel=' + 
-                        str(event.user_id) + "\nОтветить через чат: id" + str(event.user_id), vk_session, event.raw[1])
-                    #write_msg(int(event.user_id), u'Ваше сообщение переслано администратору сообщества! ;-)\nОн ответит Вам в ближайшее время.\nВ группе тестируеться бот, просим прощение за неудобства.', vk_session)
+                            if answer != "":
+                                answer = parser.sub(r'\2', str(event.text))
+                                try:
+                                    if parser.sub(r'\3', str(event.text)) != "":
+                                        write_msg(int(answer), parser.sub(r'\3', str(event.text)), vk_session)
+                                    else:
+                                        write_msg(int(answer), u".", vk_session)
+                                except Exception as e:
+                                    print(e)
+                                user_name = vk_empty_session.method('users.get', {'user_id':int(answer)})
+                                write_msg(57008075, u'Сообщение отправлено пользователю [id' + str(answer) + u'|' + 
+                                    str(user_name[0]["first_name"]) + " " + str(user_name[0]["last_name"]) + 
+                                    ']', vk_session)
+                                continue;
+                    elif event.from_chat:
+                        print(event.user_id, 'в беседе', event.chat_id)
+                    elif event.from_group:
+                        print('группы', event.group_id)
+                    try:
+                        print('Текст: ', event.text)
+                    except Exception as e:
+                        print(e)
+                    print()
+                    if not event.from_me:
+                        user_name = vk_empty_session.method('users.get', {'user_id':int(event.user_id)})
+                        write_msg(57008075, u'[id' + str(event.user_id) + u'|' + 
+                            str(user_name[0]["first_name"]) + " " + 
+                            str(user_name[0]["last_name"]) +'] написал: ' + 
+                            str(event.text) + '\nОтветить: https://vk.com/gim90818758?sel=' + 
+                            str(event.user_id) + "\nОтветить через чат: id" + str(event.user_id), vk_session, event.raw[1])
+                        #write_msg(int(event.user_id), u'Ваше сообщение переслано администратору сообщества! ;-)\nОн ответит Вам в ближайшее время.\nВ группе тестируеться бот, просим прощение за неудобства.', vk_session)
 
-            elif event.type == VkEventType.USER_TYPING:
-                print('Печатает ', end='')
+                elif event.type == VkEventType.USER_TYPING:
+                    print('Печатает ', end='')
 
-                if event.from_user:
-                    print(event.user_id)
-                elif event.from_group:
-                    print('администратор группы', event.group_id)
+                    if event.from_user:
+                        print(event.user_id)
+                    elif event.from_group:
+                        print('администратор группы', event.group_id)
 
-            elif event.type == VkEventType.USER_TYPING_IN_CHAT:
-                print('Печатает ', event.user_id, 'в беседе', event.chat_id)
+                elif event.type == VkEventType.USER_TYPING_IN_CHAT:
+                    print('Печатает ', event.user_id, 'в беседе', event.chat_id)
 
-            elif event.type == VkEventType.USER_ONLINE:
-                print('Пользователь', event.user_id, 'онлайн', event.platform)
+                elif event.type == VkEventType.USER_ONLINE:
+                    print('Пользователь', event.user_id, 'онлайн', event.platform)
 
-            elif event.type == VkEventType.USER_OFFLINE:
-                print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
+                elif event.type == VkEventType.USER_OFFLINE:
+                    print('Пользователь', event.user_id, 'оффлайн', event.offline_type)
 
-            else:
-                print(event.type, event.raw[1:])
+                else:
+                    print(event.type, event.raw[1:])
+            except requests.exceptions.ReadTimeout as e:
+                print(e)
+                time.sleep(10)
+            except requests.packages.urllib3.exceptions.ReadTimeoutError as e:
+                print(e)
+                longpoll.update_longpoll_server()
     except Exception as e:
         print(e)
-        main()
+        time.sleep(10)
+        main(longpoll)
 
 if __name__ == '__main__':
     main()
--- start.py
+++ start.py
@@ -13,7 +13,7 @@ from BotException import BotException
 
 import settings
 
-def main():
+def main(longpoll=None):
     """Описание главного файла программы
 
     Этот файл планируеться как основной стартовый модуль бота, который
@@ -25,92 +25,99 @@ def main():
     наличие у пользователя прав администратора.
 
     """
-    dyn_sett_dict = {} #Словарь динамических настроек
-    commands = {} #Словарь команд
-    rexp_commands = {} #Словарь регулярных команд
-    interfaces = {} #Словарь интерфейсов
-    vk = vk_api.VkApi(token = settings.vk_token)
-    dynamic_settings = DynamicSettings(dynamic_settings_filename = settings.dynamic_settings_file)
-    if(dynamic_settings.file_created()):
-        dyn_sett_dict = dynamic_settings.get()
-    else:
-        dyn_sett_dict = generate_standart_dynamic_setting()
-        dynamic_settings.set(dyn_sett_dict)
-
     try:
-        vk.auth()
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    vk_service = vk_api.VkApi(token = settings.service_key)
-    try:
-        vk_service.auth()
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    print('Подгружаем интерфейсы...')
-
-    print('---------------------------')
-
-    # Подгружаем плагины
-    sys.path.insert(0, settings.path)
-    for f in os.listdir(settings.path):
-        fname, ext = os.path.splitext(f)
-        if ext == '.py':
-            mod = __import__(fname)
-            interfaces[fname] = mod.Interface(vk, vk_service, dynamic_settings)
-    sys.path.pop(0)
-
-    # Регистрируем ключевые слова интерфейсов
-    for interface in interfaces.values():
-        for key, value in interface.get_keys().items():
-            commands[key] = value
-
-    #print(commands)
-
-    # Регистрируем регулярные слова интерфейсов
-    for interface in interfaces.values():
-        for key, value in interface.get_rexp().items():
-            rexp_commands[key] = value
-
-    #print(rexp_commands)
-
-    print('---------------------------')
-    print('Интерфейсы загружены')
-
-    #print(commands)
-    #print(rexp_commands)
-    print('Запускаем постоянное считывание ленты сообщений...')
-    longpoll = VkLongPoll(vk)
-    print('Считывание ленты запущено.')
-    for event in longpoll.listen():
-
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', end='')
-                print(str(event.user_id))
-                if not_sticker(vk, event):
-                    command(event, commands, rexp_commands, vk)
-            elif event.from_me:
-                print('От меня для: ', end='')
-                print(str(event.user_id))
-                
-            try:
-                print('Текст: ', event.text)
-            except UnicodeEncodeError as e:
-                print(e)
-            except KeyboardInterrupt:
-                print("Процесс бота завершен пользователем!")
-                raise
-            except Exception as e:
-                print(e)
-                raise
-            print()
+        dyn_sett_dict = {} #Словарь динамических настроек
+        commands = {} #Словарь команд
+        rexp_commands = {} #Словарь регулярных команд
+        interfaces = {} #Словарь интерфейсов
+        vk = vk_api.VkApi(token = settings.vk_token)
+        dynamic_settings = DynamicSettings(dynamic_settings_filename = settings.dynamic_settings_file)
+        if(dynamic_settings.file_created()):
+            dyn_sett_dict = dynamic_settings.get()
         else:
-            print(event.type, event.raw[1:])
+            dyn_sett_dict = generate_standart_dynamic_setting()
+            dynamic_settings.set(dyn_sett_dict)
+
+        try:
+            vk.auth()
+        except vk_api.AuthError as error_msg:
+            print(error_msg)
+            return
+
+        vk_service = vk_api.VkApi(token = settings.service_key)
+        try:
+            vk_service.auth()
+        except vk_api.AuthError as error_msg:
+            print(error_msg)
+            return
+
+        print('Подгружаем интерфейсы...')
+
+        print('---------------------------')
+
+        # Подгружаем плагины
+        sys.path.insert(0, settings.path)
+        for f in os.listdir(settings.path):
+            fname, ext = os.path.splitext(f)
+            if ext == '.py':
+                mod = __import__(fname)
+                interfaces[fname] = mod.Interface(vk, vk_service, dynamic_settings)
+        sys.path.pop(0)
+
+        # Регистрируем ключевые слова интерфейсов
+        for interface in interfaces.values():
+            for key, value in interface.get_keys().items():
+                commands[key] = value
+
+        #print(commands)
+
+        # Регистрируем регулярные слова интерфейсов
+        for interface in interfaces.values():
+            for key, value in interface.get_rexp().items():
+                rexp_commands[key] = value
+
+        #print(rexp_commands)
+
+        print('---------------------------')
+        print('Интерфейсы загружены')
+
+        #print(commands)
+        #print(rexp_commands)
+        print('Запускаем постоянное считывание ленты сообщений...')
+        if longpoll == None:
+            longpoll = VkLongPoll(vk)
+        else:
+            longpoll.update_longpoll_server()
+        print('Считывание ленты запущено.')
+        for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    print('Новое сообщение:')
+                    if event.to_me:
+                        print('Для меня от: ', end='')
+                        print(str(event.user_id))
+                        if not_sticker(vk, event):
+                            command(event, commands, rexp_commands, vk)
+                    elif event.from_me:
+                        print('От меня для: ', end='')
+                        print(str(event.user_id))
+                        
+                    try:
+                        print('Текст: ', event.text)
+                    except UnicodeEncodeError as e:
+                        print(e)
+                    except KeyboardInterrupt:
+                        print("Процесс бота завершен пользователем!")
+                        raise
+                    except Exception as e:
+                        print(e)
+                        raise
+                    print()
+                else:
+                    print(event.type, event.raw[1:])
+    except Exception as e:
+        print(e)
+        time.sleep(10)
+        main(longpoll)
     # temp_token_list = ast.literal_eval(dyn_sett_dict.get("users_token"))
     # temp_token_list.update({50583928: "{user_token: token, time_out: 10}"})
     # dyn_sett_dict.update({"users_token" : temp_token_list})
